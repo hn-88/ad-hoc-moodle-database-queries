6626d2a1e306fa39c4affde6af7f1814754216bc
hn-88 <hn-88@users.noreply.github.com>
2021-09-24 16:52:52 +0530

    Modified from Chapters viewed per user query.

---
+++ Files downloaded per user.sql
@@ -0,0 +1,15 @@
+select distinct u.id, u.idnumber, u.username, u.firstname, u.lastname, u.email, u.institution, c.shortname, r.name as "Resource viewed",  f.filename as "File downloaded" 
+from 
+{logstore_standard_log} l
+left join {course} c on l.courseid = c.id
+left join {user} u on l.userid = u.id
+left join {files} f on l.contextid = f.contextid
+left join {resource} r on l.objectid = r.id
+where
+l.timecreated > :fromdate
+and l.timecreated < :todate
+and l.action = 'viewed' 
+and l.target='course_module'  
+and ( l.objecttable = 'folder' or l.objecttable = 'resource' )
+and u.idnumber like :idnum
+and u.username like :usernameparam
