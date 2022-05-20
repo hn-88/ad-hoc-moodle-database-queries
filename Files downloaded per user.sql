7b2ff18df3edbc36657b18d1c715bafd79819d18
hn-88 <hn-88@users.noreply.github.com>
2022-05-20 15:22:38 +0530

    correct for wrong resource viewed
    resource viewed was showing wrong resource when objecttable column had value of 'folder' instead of 'resource'

--- Files downloaded per user.sql
+++ Files downloaded per user.sql
@@ -1,10 +1,12 @@
-select distinct u.id, u.idnumber, u.username, u.firstname, u.lastname, u.email, u.institution, c.shortname, r.name as "Resource viewed",  f.filename as "File downloaded" 
+select distinct u.id, u.idnumber, u.username, u.firstname, u.lastname, u.email, u.institution, c.shortname, vcs.name as "Course Module",  f.filename as "File downloaded" 
 from 
 {logstore_standard_log} l
 left join {course} c on l.courseid = c.id
 left join {user} u on l.userid = u.id
 left join {files} f on l.contextid = f.contextid
 left join {resource} r on l.objectid = r.id
+left join {course_modules} vcm on l.contextinstanceid = vcm.id 
+left join {course_sections} vcs on vcm.section = vcs.id
 where
 l.timecreated > :fromdate
 and l.timecreated < :todate
