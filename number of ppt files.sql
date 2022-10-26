6b62010fb0558173e763022b4b51a26cb144bb8e
hn-88 <hn-88@users.noreply.github.com>
2022-10-26 09:45:34 +0530

    total number and unique number

--- number of ppt files.sql
+++ number of ppt files.sql
@@ -1,3 +1,13 @@
+-- Reporting on total number of ppt files uploaded to LMS.
+
+-- References:
+
 -- https://moodle.org/mod/forum/discuss.php?d=342459
-select count(*) from {files} f
+-- https://www.w3resource.com/sql/aggregate-functions/count-function.php
+-- https://moodleschema.zoola.io/tables/files.html
+
+
+select count(*) as "All ppt(x) files",
+count(distinct f.contenthash) as "Only unique ppt(x) files"
+from {files} f
 where f.filename like '%ppt%'
