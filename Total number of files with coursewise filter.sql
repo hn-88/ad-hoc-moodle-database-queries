d718142c52ef82074e41052c595b2e0572bb0c99
hn-88 <hn-88@users.noreply.github.com>
2022-10-28 20:34:03 +0530

    adding count of pdf files

--- Total number of files with coursewise filter.sql
+++ Total number of files with coursewise filter.sql
@@ -2,7 +2,17 @@
 
 select 
 count(*) as "All teaching aids files",
-count(distinct f.contenthash) as "Only unique teaching aids files"
+count(distinct f.contenthash) as "Only unique teaching aids files",
+(select count(ff.filename) 
+from {files} ff
+  left join {context} cont2 on ff.contextid = cont2.id
+  left join {course_modules} cm on cont2.instanceid = cm.id 
+  left join {course} co2 on cm.course=co2.id
+  where 
+  cont2.contextlevel = 70
+  and ff.mimetype = 'application/pdf'
+  and co2.id=co.id) as "PDF files in course"
+
 from {files} f
   left join {context} cont on f.contextid = cont.id
   left join {course_modules} vcm on cont.instanceid = vcm.id 
