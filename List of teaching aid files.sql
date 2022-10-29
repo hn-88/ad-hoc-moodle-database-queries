9ea83959a7b87de3812d14b2fe86f5186cd9f9d0
hn-88 <hn-88@users.noreply.github.com>
2022-10-29 10:54:02 +0530

    7500 limit and more than one result in subquery
    Moodle ad hoc queries have a limit of 7500 rows. So, we need to filter by Board or standard to get a lower number.

--- List of teaching aid files.sql
+++ List of teaching aid files.sql
@@ -1,17 +1,14 @@
 -- Modified from "Sheet of coursewise number of teaching aids" query
 
 select co2.fullname as Course,
-(select
   f2.filename 
   from {files} f2
   left join {context} cont2 on f2.contextid = cont2.id
   left join {course_modules} vcm2 on cont2.instanceid = vcm2.id 
+  left join {course} co2 on co2.id = vcm2.course
   where 
-  vcm2.course=co2.id 
-  and vcm2.module=8 
+  vcm2.module=8 
   and cont2.contextlevel = 70 
   and f2.component ='mod_folder'
-   ) 
-from 
-{course} co2
-where co2.fullname like '%tandar%'
+  and co2.fullname like :board
+  and f2.filename != '.'
